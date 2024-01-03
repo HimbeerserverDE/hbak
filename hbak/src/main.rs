ca96f909e178fb0d3292eb0695ecdddda1364363
HimbeerserverDE <himbeerserverde@gmail.com>
2024-01-03 12:56:22 +0100

    implement initialization command

--- hbak/src/main.rs
+++ hbak/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Perform basic initialization of the local node.
+    Init {
+        /// The device file the local btrfs file system is located at.
+        device: String,
+        /// The name to use for this node.
+        node_name: String,
+        /// The encryption passphrase to use for owned subvolumes.
+        passphrase: String,
+    },
+}
+
+fn main() -> Result<(), hbak_common::LocalNodeError> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Init {
+            device,
+            node_name,
+            passphrase,
+        } => {
+            hbak_common::system::init(device, node_name, passphrase)?;
+        }
+    }
+
+    Ok(())
 }
