0f9b782e28500fa92c3f191f636b175de5643323
HimbeerserverDE <himbeerserverde@gmail.com>
2024-01-28 20:37:30 +0100

    hbakd: exchange synchronization info + handler parallelism

--- hbakd/src/main.rs
+++ hbakd/src/main.rs
@@ -1,8 +1,12 @@
 use hbak_common::config::NodeConfig;
 use hbak_common::conn::{AuthServ, DEFAULT_PORT};
+use hbak_common::message::SyncInfo;
+use hbak_common::proto::{LocalNode, Volume};
 use hbak_common::{LocalNodeError, NetworkError};
 
+use std::collections::HashMap;
 use std::net::{IpAddr, Ipv6Addr, SocketAddr, TcpListener, TcpStream};
+use std::thread;
 
 use clap::Parser;
 use fork::{daemon, Fork};
@@ -46,18 +50,37 @@ fn serve() -> Result<(), LocalNodeError> {
         let stream = stream?;
         let peer_addr = stream.peer_addr()?;
 
-        match handle_client(stream, &node_config) {
+        thread::spawn(move || match handle_client(stream) {
             Ok(_) => {}
             Err(e) => eprintln!("[warn] <{}> Cannot handle client: {}", peer_addr, e),
-        }
+        });
     }
 
     unreachable!()
 }
 
-fn handle_client(stream: TcpStream, node_config: &NodeConfig) -> Result<(), NetworkError> {
+fn handle_client(stream: TcpStream) -> Result<(), NetworkError> {
+    let node = LocalNode::new()?;
+
     let auth_serv = AuthServ::from(stream);
-    let (stream_conn, remote_node_auth) = auth_serv.secure_stream(node_config.auth.clone())?;
+    let (stream_conn, remote_node_auth) = auth_serv.secure_stream(node.config().auth.clone())?;
+
+    let mut local_sync_info = SyncInfo {
+        volumes: HashMap::new(),
+    };
+
+    for interest in remote_node_auth
+        .push
+        .iter()
+        .chain(remote_node_auth.pull.iter())
+    {
+        let volume = Volume::try_from(interest.as_str())?;
+        let latest_snapshots = node.latest_snapshots(volume.clone())?;
+
+        local_sync_info.volumes.insert(volume, latest_snapshots);
+    }
+
+    let (stream_conn, remote_sync_info) = stream_conn.meta_sync(local_sync_info)?;
 
     Ok(())
 }
