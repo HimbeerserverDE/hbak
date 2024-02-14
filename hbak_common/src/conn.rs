208b19bfba07105340e2046e96e8215f69e19190
HimbeerserverDE <himbeerserverde@gmail.com>
2024-02-14 17:16:36 +0100

    Revert "debug: TxError deadlock: write timeout"
    This reverts commit 1a44a1ff4cc3e99ddb8f7d1235842ee7b9cc9f23.

--- hbak_common/src/conn.rs
+++ hbak_common/src/conn.rs
@@ -297,7 +297,6 @@ impl StreamConn<Idle> {
         remote_node_name: String,
     ) -> io::Result<Self> {
         stream.set_read_timeout(Some(READ_TIMEOUT))?;
-        stream.set_write_timeout(Some(READ_TIMEOUT))?;
 
         let key = Key::from_slice(&key);
         let nonce = GenericArray::from_slice(&nonce);
