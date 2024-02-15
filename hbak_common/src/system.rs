2c44c7e152cd3201919dd70d6979d99b0660b9e9
HimbeerserverDE <himbeerserverde@gmail.com>
2024-02-15 17:16:45 +0100

    switch from MIT to AGPL-3.0

--- hbak_common/src/system.rs
+++ hbak_common/src/system.rs
@@ -1,3 +1,19 @@
+// hbak_common is the main hbak library implementing the protocol shared logic.
+// Copyright (C) 2024  Himbeer <himbeerserverde@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use crate::config::NodeConfig;
 use crate::proto::{BACKUP_DIR_C, SNAPSHOT_DIR_C};
 use crate::LocalNodeError;
